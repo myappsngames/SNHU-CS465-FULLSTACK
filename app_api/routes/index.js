62895465bbf009e39377d068717c923a62c54076
Dominick Marquez <myappsngames@outlook.com>
2024-10-09 18:06:49 -0700

    Module 5 completed baseline

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -5,9 +5,22 @@ const router = express.Router(); // Express router logic
 const tripsController = require('../controllers/trips');
 
 // define route for our trips endpoint
-router.get('/trips', tripsController.tripsList); // GET Method routes tripList
+//router.get('/trips', tripsController.tripsList); // GET Method routes tripList
 
 // define route for our trips/:tripCode endpoint
-router.get('/trips/:tripCode', tripsController.tripsFindByCode); // GET Method routes tripsFindByCode - requires parameter
+//router.get('/trips/:tripCode', tripsController.tripsFindByCode); // GET Method routes tripsFindByCode - requires parameter
+
+// define route for out trips endpoint
+//router.get('/', ctrlMain.index);
+
+// define route for our trips endpoint
+router
+    .route('/trips')
+    .get(tripsController.tripsList); // GET Method routes tripList
+    
+// GET Method routes tripsFindByCode - requires parameter
+router
+    .route('/trips/:tripCode')
+    .get(tripsController.tripsFindByCode); // GET Method routes tripFindByCode
 
 module.exports = router;
