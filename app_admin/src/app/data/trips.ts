6a430011f0e0b737dd17233996c7b8011c1250f4
Dominick Marquez <myappsngames@outlook.com>
2024-10-10 20:42:44 -0700

    Module 6 Completed Baseline

---
+++ app_admin/src/app/data/trips.ts
@@ -0,0 +1,30 @@
+export const trips = [
+    {
+        "code": "GALR210214",
+        "name": "Gale Reef",
+        "length": "4 nights / 5 days",
+        "start": "2021-02-14T08:00:00Z",
+        "resort": "Gale Bay",
+        "perPerson": "799.00",
+        "image": "reef1.jpg",
+        "description" : "<p>Gale Reef is a reef in the Gale Bay, located in the northwestern part of the island of New Zealand. It is the largest of the three reefs in the bay, and is home to a wide variety of marine life, including sharks, rays, and turtles. The reef is also a popular destination for scuba divers, who can explore its underwater world and discover its unique ecosystem.</p>"
+    }, {
+        "code": "DAWR210315",
+        "name": "Dawson's Reef",
+        "length": "4 nights / 5 days",
+        "start": "2021-03-15T08:00:00Z",
+        "resort": "Blue Lagoon, 4 stars",
+        "perPerson": "1199.00",
+        "image": "reef2.jpg",
+        "description" : "<p>Dawson's Reef is a reef in the Gale Bay, located in the northwestern part of the island of New Zealand. It is the largest of the three reefs in the bay, and is home to a wide variety of marine life, including sharks, rays, and turtles. The reef is also a popular destination for scuba divers, who can explore its underwater world and discover its unique ecosystem.</p>"
+    }, {
+        "code": "CLAR210621",
+        "name": "Claire's Reef",
+        "length": "4 nights / 5 days",
+        "start": "2021-06-21T08:00:00Z",
+        "resort": "Coral Sands, 5 stars",
+        "perPerson": "1999.00",
+        "image": "reef3.jpg",
+        "description" : "<p>The Great Barrier Reef is the world's largest coral reef system, covering an area of over 2,900 square kilometers. It is located in the Coral Sea, and is home to a wide variety of marine life, including sharks, rays, and turtles. The reef is also a popular destination for scuba divers, who can explore its underwater world and discover its unique ecosystem.</p>"
+    }
+];
--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -5,19 +5,11 @@ const router = express.Router(); // Express router logic
 const tripsController = require('../controllers/trips');
 
 // define route for our trips endpoint
-//router.get('/trips', tripsController.tripsList); // GET Method routes tripList
-
-// define route for our trips/:tripCode endpoint
-//router.get('/trips/:tripCode', tripsController.tripsFindByCode); // GET Method routes tripsFindByCode - requires parameter
-
-// define route for out trips endpoint
-//router.get('/', ctrlMain.index);
-
-// define route for our trips endpoint
 router
     .route('/trips')
-    .get(tripsController.tripsList); // GET Method routes tripList
-    
+    .get(tripsController.tripsList) // GET Method routes tripList
+    .post(tripsController.tripsAddTrip); // POST Method Adds a Trip
+
 // GET Method routes tripsFindByCode - requires parameter
 router
     .route('/trips/:tripCode')
